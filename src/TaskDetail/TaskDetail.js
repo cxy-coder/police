ac8deb51e7128f056a98fca1fe0cccce6f592e99
znanjie <1016224076@qq.com>
2019-06-16 18:49:49 +0800

    任务详情页弹窗控制

--- src/TaskDetail/TaskDetail.js
+++ src/TaskDetail/TaskDetail.js
@@ -4,8 +4,22 @@ export default class TaskDetail extends Laya.Script {
     }
     onEnable() {
         console.log(this);
+        this.dialogControl(false);//隐藏提示弹窗
     }
     onClick() {
         console.log(this);
+    }
+    dialogControl(val) {
+        const dialogTipsArr = [
+            this.owner.btnClose,
+            this.owner.btnConfirm,
+            this.owner.btnNo,
+            this.owner.btnYes,
+            this.owner.dialogContent,
+            this.owner.dialogTips
+        ]
+        dialogTipsArr.forEach(element => {
+            element._visible = val;
+        });
     }
 }
