38b5edbe9309c92373777dc71483138ab2a677d0
znanjie <1016224076@qq.com>
2019-06-17 20:29:43 +0800

    Loading进度

--- src/Loading.js
+++ src/Loading.js
@@ -5,8 +5,11 @@ export default class Loading extends Laya.Script {
     onEnable() {
         console.log(this);
         let timer = null;
+        let count = 0;
         timer = setInterval(() => {
             this.owner.loadingCar.y = this.owner.loadingCar.y + 5;
+            count += 0.317;
+            this.owner.loadingText.text = `Loading... ${parseInt(count)}%`
             if (this.owner.loadingCar.y > 1334) {
                 clearInterval(timer);
                 Laya.Scene.open('GamePage.scene');
